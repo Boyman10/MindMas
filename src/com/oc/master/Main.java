8ce2a63dc0c2880483f4c45743bfb8920768b9c0
Eddy Mio <contact@remotehireman.fr>
2018-01-05 12:27:36 +0100

    Project initialization

---
+++ src/com/oc/master/Main.java
@@ -0,0 +1,18 @@
+package com.oc.master;
+
+/**
+ * Main class to launch program via console or Swing or FX
+ * @author bob
+ * @version 1.0.1
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
