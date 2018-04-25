56046aa4132c646b354f04f4102fa2891e0f82db
Eddy Mio <contact@remotehireman.fr>
2018-04-25 17:14:05 +0200

    Working on log4j2 utility class to reconfigure the logger in case of debug mode passed in parameter of the application.

--- src/com/oc/master/utils/MyLogger.java
+++ src/com/oc/master/utils/MyLogger.java
@@ -1,7 +1,11 @@
 package com.oc.master.utils;
 
+import org.apache.commons.configuration2.Configuration;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.LoggerContext;
 
 /**
  * Gathering logger implementation
@@ -20,5 +24,16 @@ public class MyLogger {
 		
 		return logger;
 		
+	}
+	
+	/**
+	 * Reconfiguring the logger depending on parameter
+	 * case debug mode is enabled - not the default !
+	 */
+	public static void reconfigureLogger() {
+		
+		LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+		Configuration config = ctx.getConfiguration();
+		config.addLogger("com", Level.INFO, (Filter) null);
 	}
 }
