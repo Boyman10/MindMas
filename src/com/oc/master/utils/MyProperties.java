e63193f39eaceecffa2cd69ceaa2dd350b45e745
Eddy Mio <contact@remotehireman.fr>
2018-04-25 14:40:18 +0200

    Adding apache commons dependency by copy/paste into lib folder and adding to buildpath

--- src/com/oc/master/utils/MyProperties.java
+++ src/com/oc/master/utils/MyProperties.java
@@ -7,5 +7,30 @@ package com.oc.master.utils;
  */
 public class MyProperties {
 
+	/**
+	 * Constructor class
+	 */
+	public MyProperties() {
+		
+		
+	}
+	
+	public void readProperties() {
+		
+		Parameters params = new Parameters();
+		FileBasedConfigurationBuilder<FileBasedConfiguration> builder =
+		    new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+		    .configure(params.properties()
+		        .setFileName("usergui.properties"));
+		try
+		{
+		    Configuration config = builder.getConfiguration();
+		    ...
+		}
+		catch(ConfigurationException cex)
+		{
+		    // loading of the configuration file failed
+		}
+	}
 	
 }
